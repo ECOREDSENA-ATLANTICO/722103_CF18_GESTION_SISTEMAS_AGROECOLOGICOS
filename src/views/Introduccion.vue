9ec16014d64a6e944a67db7f844a5af1a222a248
AndresDiazPinto <andresvis565@gmail.com>
2024-07-18 11:09:39 -0500

    Ajustes
    Se realiza ajuste en el link de video, se actualizan CFA, se empaqueta CF

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -13,8 +13,7 @@
 
     figure
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/hre-GxrjLnc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-    
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/8XcCF9PK7EU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
       
     
 </template>
